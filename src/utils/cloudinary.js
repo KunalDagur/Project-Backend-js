079ad0d72ea5c2596973230b948507e7f6233e95
KunalDagur <kdagur34@gmail.com>
2024-08-15 14:51:22 +0530

    Middlewares file setup

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs, { unlink, unlinkSync } from "fs";
+import { type } from "os";
+
+// Configuration
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+});
+
+const uploadOnCloudinary = async (localfilepath)=>{
+try {
+    if(!localfilepath){
+        return null;
+    }else{
+       const response = await  cloudinary.uploader.upload(localfilepath, {
+            resource_type: "auto"
+        });
+        console.log("File has been upload successfully", response.url);
+        return response;
+    }
+} catch (error) {
+    fs.unlinkSync(localfilepath); //Will remove the temp file as upload operation on cloudinary has failed 
+    return null;
+}
+}
+
+export {uploadOnCloudinary}
